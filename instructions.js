caf3b2faee2a2149abb6cf460b9ade4a2df4de48
Rachmat Priambudi <rachmat.keybladers93@gmail.com>
2020-01-16 11:55:08 +0700

    export default email template for forget password

--- instructions.js
+++ instructions.js
@@ -8,6 +8,11 @@ module.exports = async (cli) => {
         const configOut = path.join(cli.helpers.configPath(), 'ramenauth.js')
         await cli.command.copy(configIn, configOut)
         cli.command.completed('create', 'config/ramenauth.js')
+
+        const emailTemplateIn = path.join(__dirname, './src/views', 'forgot.edge')
+        const emailTemplateOut = path.join(cli.helpers.viewsPath(), 'emails/forgot.edge')
+        await cli.command.copy(emailTemplateIn, emailTemplateOut)
+        cli.command.completed('create', 'emails/forgot.edge')
     } catch (error) {
         // ignore error
         console.log(error)
